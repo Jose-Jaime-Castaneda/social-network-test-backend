ab0f2d987e93fa2c475e9ad1a66e213a767d409a
Jose-Jaime-Castaneda <josecastanena@gmail.com>
2024-02-21 09:38:49 -0600

    último cambio

--- src/routes/publication.js
+++ src/routes/publication.js
@@ -9,7 +9,7 @@ const publicationController = require('../controllers/publication');
 // Rutas GET ALL
 router.get('/detail/:id', auth.auth, publicationController.detail);
 router.get('/publications/:id?/:page?', auth.auth, publicationController.getUserPublications);
-router.get('/get-img/:file', auth.auth, publicationController.getImgPublication);
+router.get('/get-img/:file', publicationController.getImgPublication);
 router.get('/all-publications/:page?', auth.auth, publicationController.getPublications);
 router.get('/feed/:page?', auth.auth, publicationController.getFeed);
 // Rutas POST
